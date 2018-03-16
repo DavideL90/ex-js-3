1e02dd62191a5884b84824d19767425559790ef1
DavideL90 <davide.lecci@icloud.com>
2018-03-16 16:27:28 +0100

    Es radar started and objects created

---
+++ Es. radar/main.js
@@ -0,0 +1,14 @@
+var aereiDescr = {
+                    "numAereo" : ["AZ890", "LO452", "PP326"],
+                    "lat" : [19.890, 18.980, 16.120],
+                    "long" : [11.250, 17.555, 13.986],
+                    "carbu" : [1000, 1200, 1100],
+                    "km/kg" : [100, 150, 125]
+                  };
+var listaAeroporti ={
+                      "nomeAeroporto" : ["Pisa", "Bergen", "Madrid"],
+                      "lat" : [18.890, 17.222, 15.325],
+                      "long" : [20.365, 14.356, 16.458]
+                    }
+console.log(aereiDescr);
+console.log(listaAeroporti);
