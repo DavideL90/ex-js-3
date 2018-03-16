15e0031f35d059000d4abf8ae073e34df127730b
DavideL90 <davide.lecci@icloud.com>
2018-03-16 18:00:32 +0100

    completed

--- Es. radar/main.js
+++ Es. radar/main.js
@@ -1,14 +1,40 @@
 var aereiDescr = {
-                    "numAereo" : ["AZ890", "LO452", "PP326"],
-                    "lat" : [19.890, 18.980, 16.120],
-                    "long" : [11.250, 17.555, 13.986],
-                    "carbu" : [1000, 1200, 1100],
-                    "km/kg" : [100, 150, 125]
+                    "numAereo" : ["AZ890", "LO452", "PP326" , "XD987"],
+                    "lat" : [19.890, 18.980, 16.120 , 17.115],
+                    "long" : [11.250, 17.555, 13.986, 18.464],
+                    "carbu" : [10, 1200, 1100, 1150],
+                    "km1kg" : [10, 150, 125, 200]
                   };
 var listaAeroporti ={
                       "nomeAeroporto" : ["Pisa", "Bergen", "Madrid"],
                       "lat" : [18.890, 17.222, 15.325],
                       "long" : [20.365, 14.356, 16.458]
                     }
-console.log(aereiDescr);
-console.log(listaAeroporti);
+
+//Determino la lunghezza degli array contenuti negli oggetti
+var lengthOfLatAereiDescr = Object.keys(aereiDescr.lat).length;
+var lengthOfLatAereoporti = Object.keys(listaAeroporti.lat).length;
+var distanza = []; //creo un array per contenere le distanze degli aerei
+
+//adesso creo un ciclo for per calcolare la distanza che ogni aereo ha rispetto ad ogni aeroporto
+for (var i = 0; i < lengthOfLatAereoporti; i++) {
+  for (var x = 0; x < lengthOfLatAereiDescr; x++) {
+    distanza = distance(aereiDescr.lat[x], aereiDescr.long[x], listaAeroporti.lat[i], listaAeroporti.long[i]);
+    if(distanza > (aereiDescr.carbu[x] * aereiDescr.km1kg[x])){
+      alert("L'aereo " + aereiDescr.numAereo[x] + " non arriverà a: " + listaAeroporti.nomeAeroporto[i] + " non ha abbastanza carburante! Morirete tutti!!!");
+    }
+    else{
+      alert("Il carburante dell'aereo: " + aereiDescr.numAereo[x] + " è più che sufficiente! Dormite tranquilli")
+    }
+  }
+}
+
+function distance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
